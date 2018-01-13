750cb3e45af5744c3e13c68d4047b9926164ad3e
TommyDeblay <deblay.thomas@gmail.com>
2018-01-13 17:36:24 +0100

    try to fix database issues

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyCNydWVL4fiPg7v-2NMaOj1AiGfbyemLTw",
+  authDomain: "woulo-8d4c5.firebaseapp.com",
+  databaseURL: "https://woulo-8d4c5.firebaseio.com",
+  projectId: "woulo-8d4c5",
+  storageBucket: "woulo-8d4c5.appspot.com",
+  messagingSenderId: "763233643775"
 };
 
 firebase.initializeApp(config);
