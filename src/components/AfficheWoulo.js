e039af70ddf2fb592a0723e4c5da20791944c139
TommyDeblay <deblay.thomas@gmail.com>
2018-01-16 11:52:12 +0100

    afficheWoulo update font end

--- src/components/AfficheWoulo.js
+++ src/components/AfficheWoulo.js
@@ -6,7 +6,10 @@ const AfficheWoulo = (props) => (
         <div className="row affichewoulo" >
             <div className="login-form" >
                 <div className="login-form__box">
-                    {props.selectedWoulo && <h5 className="login-form__title " > Woulooooo Ba mwen bagay la, woulooo Ba mwen bagay la, Woulooooo ba mwen bagay la , blablablablabla  </h5>}
+                    {props.selectedWoulo && <h5 className="login-form__title " > Woulo ban mwen babgay'la! <br />
+                    Woulo ban mwen babgay'la! <br />
+                    Woulo ban mwen babgay'la! <br />
+                    Tonnè di dié ban mwen bagay'la!   </h5>}
                     {props.selectedWoulo && <p className="lewoulo" > {props.selectedWoulo.woulo}</p>}
                    
                  </div>
